37e2c3911205ef0da38c372d8b5097a1fb611de0
Alec Wong <aw685@cornell.edu>
2018-03-15 10:51:47 -0400

    Update functions to include one cluster maker, not finished yet though

--- functions.R
+++ functions.R
@@ -1,6 +1,6 @@
 # Host for functions used in camera trapping simulation.
 
-# Remote task read/write section ------------------------------------------------------------------------------
+# Remote task read/write section ---------------------------------------------------------------------------------------------------------------------------------------------
 
 # Need a function to read task ID's, whether in progress, whether output saved (or code with error), and owner
 
@@ -55,5 +55,79 @@ taskSelect = function(taskList){
   freeTaskIndex = taskList$inProgress == 0 & taskList$completed == 0
   
   freeTasks = taskList$taskID[freeTaskIndex]
+  
+}
+
+# Design section ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Need functions for arbitrary specification of designs.
+
+getDesign = function(sigma = 1, noTraps, noClust, xlim, ylim){
+  
+  # Under construction
+  
+  # State space will be a set of limiting values for a rectangle
+  
+  # My opinion is that we should simulate within the set of {2, 4, 5, 10, 20,
+  # 25, 50} clusters to avoid arbitrary placement of singular traps. The effect
+  # of optimal placement of singular traps will confound interpretation, I
+  # think, and the marginal effect of a singular trap elsewhere in a grid
+  # barring some adaptive process is likely very small.
+  
+  # Need algorithm to create a cluster of equally spaced traps 
+  
+  # Probably want a rectangular, or hexagonal(|triangular) approach, allowing
+  # compact structures that are also equidistant
+  
+  trapsPerClust = noTraps / noClust
+  
+  
+  
+  
+  return(NULL)
+  
+}
+
+makeTriCluster = function(noPoints,spacing){
+  
+  # Make cluster in triangular grid fashion
+  points = matrix(data = NA, nrow = nPoints, ncol = 2)
+  
+  return(points)
+  
+}
+
+makeRectCluster = function(){
+  
+  # Make cluster in rectangular fashion
+  
+  points = matrix(data = NA, nrow = nPoints, ncol = 2)
+  
+  return(points)
+  
+}
+
+
+# Deprecated -------------------------------------------------------------------------------------------------------------------------------------
+
+makeVogelCluster = function(concCircles, noPoints, angle = NULL){
+  
+  # Makes a cluster of roughly equidistant points inside a unit disc.
+  # Probably not great use for small number of points.
+  # Probably hard to constrain within-cluster distances.
+  
+  if(is.null(angle)){angle = pi * (3 - sqrt(5))}
+  
+  points = matrix(data = NA, nrow = noPoints, ncol = 2)
+  
+  for(p in 1:noPoints){
+    
+    theta = p * angle
+    r = sqrt(p) / sqrt(noPoints)
+    points[p,] = c(r*cos(theta), r*sin(theta))
+  }
+  
+  return(points)
+  
   
 }
--- test.R
+++ test.R
@@ -1,1 +1,28 @@
-taskIO(debug = F, taskURL = 'testTaskList.csv')
+library(ggplot2)
+library(dplyr)
+
+
+# Testing taskIn
+
+taskIN(debug = F, taskURL = 'taskList.csv')
+
+
+# Testing getDesign
+
+xlim = c(0,100)
+ylim = c(0,100)
+
+getDesign(noTraps = 100, noClust = 50)
+
+# Testing make Vogelcluster
+
+points = makeVogelCluster(noPoints = 20)
+
+points = data.frame(points)
+names(points) = c('x', 'y')
+
+ggplot(data = points) + 
+  geom_point(aes(x = x, y = y)) + 
+  coord_equal()
+
+summary(as.numeric(fields::rdist(points)))
