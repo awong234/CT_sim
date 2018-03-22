b9aaa881ea61ec1c01b9e75123af9f41bffcf427
awong234 <aw685@cornell.edu>
2018-03-22 16:12:19 -0400

    Simplified script, included warning of reset task list state.

--- test.R
+++ test.R
@@ -6,8 +6,11 @@ source('functions.R')
 
 # Testing task list functions -------------------------------------------------------------------------
 
-# Full test
+# Show table
 
+printDB() %>% filter(owner == Sys.info()['nodename'])
+
+# Full test
 
 reservedTasks = reserveTasks()
 printDB()
@@ -21,12 +24,10 @@ while(length(reservedTasks) > 0){
 
 
 
-
+# WARNING: RESETS STATE OF TASK LIST COMPLETELY
 # Generate test task list
 
-testTaskList = data.frame("taskID" = seq(from = 1, to = 100), "inProgress" = 0, "completed" = 0, "owner" = NA)
-testTaskList = rbind.data.frame(testTaskList, data.frame("taskID" = c(101,102, 103), 
-                                                         "inProgress" = c(1,1,0), "completed" = c(0,0,1), "owner" = c(Sys.info()['nodename'], "other", "other")))
+testTaskList = data.frame("taskID" = seq(from = 1, to = 1000), "inProgress" = 0, "completed" = 0, "owner" = "NONE")
 row.names(testTaskList) = NULL
 write.csv(testTaskList, file = '../CT_sim_tasks/taskList.csv', row.names = F)
 rm(testTaskList)
@@ -35,10 +36,6 @@ rm(testTaskList)
 
 con <- dbConnect(odbc::odbc(), .connection_string = "Driver={SQL Server};Server=den1.mssql6.gear.host;Database=tasklistntres;Uid=tasklistntres;Pwd=Gy435_eN5-Ry;")
 
-# Read table 
-
-(taskList = dbReadTable(conn = con, name = 'tasklistntres'))
-
 # Load test table into db
 
 taskList = read.csv(file = "../CT_sim_tasks/taskList.csv")
@@ -46,17 +43,6 @@ taskList = read.csv(file = "../CT_sim_tasks/taskList.csv")
 dbWriteTable(conn = con, name = 'tasklistntres', value = taskList, overwrite = TRUE)
 
 dbDisconnect(con)
-
-# update table to reserve tasks
-
-reservedTasks = seq(1:8)
-
-dbExecute(conn = con, statement = paste0("UPDATE tasklistntres SET inProgress = 1, owner = \'",Sys.info()['nodename'],"\' WHERE taskID IN (", toString(reservedTasks), ");"))
-
-# Did it work? It DID!
-
-dbReadTable(conn = con, name = 'tasklistntres')
-
 
 
 # Testing getDesign --------------------------------------------------------
