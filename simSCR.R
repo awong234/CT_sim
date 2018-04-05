8cde46b8b17268036f92df3a7762f4aac88a42b9
Royle <aroyle@usgs.gov>
2018-04-05 11:28:41 -0400

    updates to data simulator function

--- simSCR.R
+++ simSCR.R
@@ -1,41 +1,56 @@
-e2dist<-function (x, y)
-{
-  i <- sort(rep(1:nrow(y), nrow(x)))
-  dvec <- sqrt((x[, 1] - y[i, 1])^2 + (x[, 2] - y[i, 2])^2)
-  matrix(dvec, nrow = nrow(x), ncol = nrow(y), byrow = F)
-}
+simSCR<- 
+ function(N=120,p0=2,sigma=0.50,K=10,X ,buff=3,thinning.rate1 = 0.7, thinning.rate2=0.7){
 
+## lamd<- lam0*exp(-D*D/(2*sigma*sigma)) 
+##y.used[i,j,k]=rpois(1, lamd) #how many times was the site used?
+##y.occ[i,j,k]=rbinom(1, y.used[i,j,k], prob=thinning.rate2) #did individual i get detected at trap j on occasion k
+##y.SCR[i,j,k]=rbinom(1, y.occ[i,j,k], prob=thinning.rate) #could it be identified
 
-#' Simulate data from a SCR study. Modified from SPIM package
-#' @param N a vector indicating the number of individuals to simulate
-#' @param p0 the detection function baseline detection probability
-#' @param sigma the spatial scale parameter
-#' @param K the number of capture occasions
-#' @param X the K x 2 matrix of trap locations
-#' @param buff the distance to buffer the trapping array in the X and Y dimensions to produce the state space
-#' @return a list containing the capture history, activity centers, trap object, and several other data objects and summaries.
-#' @description This function simulates data from a camera trap SCR study. The extent of the state space is controlled by "buff", which buffers the
-#' minimum and maximum X and Y extents.  Therefore, it probably only makes sense for square or rectangular grids.  Functionality
-#' for user-provided polygonal state spaces will be added in the future.
-#' @author Ben Augustine
-#' @export
+ xmin<- min(X[,1])-buff
+ xmax<- max(X[,1])+buff
+ ymin<- min(X[,2])-buff
+ ymax<- max(X[,2])+buff
+ Xgrid<- expand.grid(seq(xmin,xmax,,50), seq(ymin,ymax,,50))
 
-simSCR <-
-  function(N=120,p0=0.2,sigma=0.50,K=10,X=X,buff=3,obstype="bernoulli"){
-    # # simulate a population of activity centers
+    # # simulate USAGE for a population of activity centers
     s<- cbind(runif(N, min(X[,1])-buff,max(X[,1])+buff), runif(N,min(X[,2])-buff,max(X[,2])+buff))
-    D<- e2dist(s,X)
+    D<- e2dist(s,rbind(X,Xgrid) )
     pd<- p0*exp(-D*D/(2*sigma*sigma))
-    J<- nrow(X)
+    J<- nrow(X) + nrow(Xgrid)
     # Simulate encounter history
-    y <-array(0,dim=c(N,J,K))
+    y.used <-array(0,dim=c(N,J,K))
     for(i in 1:N){
       for(j in 1:J){
         for(k in 1:K){
-          y[i,j,k]=rbinom(1,1,pd[i,j])
+          y.used[i,j,k]=rpois(1,pd[i,j])
         }
       }
     }
+ 
+ y.used<- y.used[1:N, 1:nrow(X), 1:K]
+
+ # compute total occupancy on the landscape by evaluating on a fine grid (Xgrid above)
+     p.grid<- pd[1:N, (nrow(X)+1):J]
+     H.grid<- colSums(p.grid)
+     psi.grid<- 1-exp(-H.grid)
+
+ # compute total occupancy probability on the trap locations
+    pd<- pd[1:N, 1:nrow(X)]
+    H.total<- colSums(pd)
+    psi<- 1-exp(-H.total)
+
+ # Now compute the SCR data:
+    J<- nrow(X)
+    y.occ<- y.scr <-array(0,dim=c(N,J,K))
+    for(i in 1:N){
+      for(j in 1:J){
+        for(k in 1:K){
+          y.occ[i,j,k]=rbinom(1, y.used[i,j,k], prob=thinning.rate1)
+          y.scr[i,j,k]=rbinom(1, y.occ[i,j,k], prob=thinning.rate2)
+        }
+      }
+    }
+    y<- y.scr
     caps=apply(y,1,sum)
     idx=order(caps,decreasing=TRUE)
     y=y[idx,,]
@@ -48,6 +63,9 @@ simSCR <-
     scaps[scaps>0]=scaps[scaps>0]-1
     nscap=sum(scaps>0)
     sumscap=sum(scaps)
-    out<-list(y=y,s=s,X=X, K=K,n=n,nscap=nscap,sumscap=sumscap,buff=buff,obstype=obstype)
+    out<-list(y.used=y.used,y.occ=y.occ,y.scr=y.scr,s=s,X=X, K=K,n=n,nscap=nscap,sumscap=sumscap,buff=buff,
+ psi.bar = mean(psi.grid) )
     return(out)
   }
+ 
+data<-simSCR(X=X)
