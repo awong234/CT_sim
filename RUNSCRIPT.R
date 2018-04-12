ea31b6b1e6610f598aee69f399bfa063a37ae738
awong234 <aw685@cornell.edu>
2018-04-12 09:24:34 -0400

    Added RUNSCRIPT.R, the eventual one-click script for distributed analysis. Added writeSettings.R, which writes all of the settings to be analyzed to a file.

---
+++ RUNSCRIPT.R
@@ -0,0 +1,17 @@
+# Main script housing preparation of data, analysis of data, and exporting analysis elements.
+
+# Sections 
+
+# Preparation
+  # Extract settings
+  # Generate grids
+  # Generate SCR data
+  # Generate OCC data
+# Analysis
+  # Analyze data under SCR
+  # Analyze data under OCC
+  # Export analysis elements
+
+
+# Extract Settings ------------------------------------------------------------------------------------
+
