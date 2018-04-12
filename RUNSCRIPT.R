b10ebaa3feb88879f08fd39238c82ebcb6f50823
awong234 <aw685@cornell.edu>
2018-04-12 11:16:07 -0400

    Implementation skeleton to be filled in with appropriate functions for analysis.

--- RUNSCRIPT.R
+++ RUNSCRIPT.R
@@ -12,6 +12,67 @@
   # Analyze data under OCC
   # Export analysis elements
 
+library(doParallel)
+
+source('build.cluster.alt.R')
+source('simSCR.R')
+source('functionsSQL.R')
+
+numTasks = detectCores() - 1 # how many concurrent analyses to be done?
+
+registerDoParallel(cores = numTasks) # editable with numTasks
 
 # Extract Settings ------------------------------------------------------------------------------------
+
+extract = function(what){invisible(Map(f = function(x,y){assign(x = x, value = y, envir = .GlobalEnv)}, x = names(what), y = what))}
+
+# Example
+# Read in settings table
+settings = read.csv(file = 'settings.csv', stringsAsFactors = F)
+
+extract(settings[1,])
+
+# Reserve some tasks to be completed.
+reservedTasks = reserveTasks(numTasks = numTasks)
+
+while(reservedTasks > 0){
+  
+  foreach(i = reservedTasks, .export = c("updateTaskCompleted", "simSCR", "build.cluster.alt", "extract")) %dopar% {
+    
+    settingsLocal = settings[i,] # Extract settings for task i
+    
+    extract(settingsLocal) # Assign all components (p0, lam0, etc.) to scoped to FUNCTION environment - won't affect other tasks.
+    
+    # Generate trap array ---------------------------------------------------------------------------------
+    X = build.cluster.alt(ntraps = nTraps, ntrapsC = ntrapsC, spacingin = spacingin, spacingout = spacingout)
+    
+    # Simulate activity centers ---------------------------------------------------------------------------
+    
+    # Either a fixed population per design choice, or fully random populations 
+    # Right now all done in simSCR()
+    
+    # Simulate encounters ---------------------------------------------------------------------------------
+    scrData = simSCR()
+    
+    # Gather data into analysis tool (occupancy and SCR) --------------------------------------------------
+    
+    # Format properly . . . 
+    
+    # Perform analysis . . . 
+    
+    # ANALYSIS i . . . 
+    
+    # Write result to output directory of choice . . . 
+    
+    # Note completion on server
+    updateTaskCompleted(reservedTasks = i)
+    
+    # Reserve some more tasks 
+    reservedTasks = reserveTasks(numTasks = numTasks)
+    
+  }
+  
+}
+
+
 
