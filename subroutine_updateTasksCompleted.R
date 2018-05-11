67e865ce661c1d1fb3b833ded33be6fbb3d7fe91
Alec Wong <aw685@cornell.edu>
2018-05-11 12:16:21 -0400

    ADD subroutine to update tasks completed.
    * Will not make main process wait. Updates every 10 minutes.

---
+++ subroutine_updateTasksCompleted.R
@@ -0,0 +1,33 @@
+source('functionsSQL.R')
+
+subrt_updateComplete = function(){
+  
+repeat{
+  
+  # Check localOuput/ folder for tasks that are done
+  
+  files = dir(path = 'localOutput', pattern = ".Rdata")
+  matches = (regmatches(x = files, m = gregexpr(pattern = '\\d+', text = files, perl = T)))
+  done = do.call(what = rbind, args = lapply(matches, as.integer))
+  
+  # Compare against server and find which ones are not done up there
+  
+  con <- dbConnect(odbc::odbc(), .connection_string = "Driver={SQL Server};Server=den1.mssql6.gear.host;Database=tasklistntres;Uid=tasklistntres;Pwd=Gy435_eN5-Ry;")
+  
+  statement = paste0("SELECT taskID FROM tasklistntres WHERE owner = \'", Sys.info()['nodename'], "\' AND inProgress = 1")
+  
+  out = dbGetQuery(conn = con, statement = statement)
+  
+  tasksToComplete = done[!done %in% out]
+  
+  # Mark completed
+  
+  updateTaskCompleted(reservedTasks = tasksToComplete)
+  
+  Sys.sleep(10*60)
+  
+}
+
+}
+
+subrt_updateComplete()
