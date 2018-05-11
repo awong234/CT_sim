11ca6c29899e1ee97aed519803563aed7bc27820
Alec Wong <aw685@cornell.edu>
2018-05-11 12:44:47 -0400

    Added subroutine.bat.
    subroutine_updateTasksCompleted.R will, every 10 minutes,
    
    * Check to see what tasks exist in the output files
    * Compare against the tasks marked 'inProgress = 1' on the server
    * Update those to be complete.

--- subroutine_updateTasksCompleted.R
+++ subroutine_updateTasksCompleted.R
@@ -1,32 +1,35 @@
 source('functionsSQL.R')
 
 subrt_updateComplete = function(){
-  
-repeat{
-  
-  # Check localOuput/ folder for tasks that are done
-  
-  files = dir(path = 'localOutput', pattern = ".Rdata")
-  matches = (regmatches(x = files, m = gregexpr(pattern = '\\d+', text = files, perl = T)))
-  done = do.call(what = rbind, args = lapply(matches, as.integer))
-  
-  # Compare against server and find which ones are not done up there
-  
-  con <- dbConnect(odbc::odbc(), .connection_string = "Driver={SQL Server};Server=den1.mssql6.gear.host;Database=tasklistntres;Uid=tasklistntres;Pwd=Gy435_eN5-Ry;")
-  
-  statement = paste0("SELECT taskID FROM tasklistntres WHERE owner = \'", Sys.info()['nodename'], "\' AND inProgress = 1")
-  
-  out = dbGetQuery(conn = con, statement = statement)
-  
-  tasksToComplete = done[!done %in% out]
-  
-  # Mark completed
-  
-  updateTaskCompleted(reservedTasks = tasksToComplete)
-  
-  Sys.sleep(10*60)
-  
-}
+
+  print("DON'T CLOSE ME, I AM MONITORING TASKS AND UPLOADING THEM EVERY 10 MINUTES")
+    
+  repeat{
+    
+    # Check localOuput/ folder for tasks that are done
+    
+    files = dir(path = 'localOutput', pattern = ".Rdata")
+    matches = (regmatches(x = files, m = gregexpr(pattern = '\\d+', text = files, perl = T)))
+    done = do.call(what = rbind, args = lapply(matches, as.integer))
+    
+    # Compare against server and find which ones are not done up there
+    
+    con <- dbConnect(odbc::odbc(), .connection_string = "Driver={SQL Server};Server=den1.mssql6.gear.host;Database=tasklistntres;Uid=tasklistntres;Pwd=Gy435_eN5-Ry;")
+    
+    statement = paste0("SELECT taskID FROM tasklistntres WHERE owner = \'", Sys.info()['nodename'], "\' AND inProgress = 1")
+    
+    out = dbGetQuery(conn = con, statement = statement)
+    
+    tasksToComplete = done[!done %in% out]
+    
+    # Mark completed
+    if(length(tasksToComplete > 0)) # updateTaskCompleted(reservedTasks = tasksToComplete)
+    
+    print(paste0("Updated ", length(tasksToComplete), " tasks."))
+    
+    Sys.sleep(10*60)
+    
+  }
 
 }
 
