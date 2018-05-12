91db6899996a438765ea533e194a1f74ca40374b
Alec Wong <aw685@cornell.edu>
2018-05-12 15:38:22 -0400

    subroutine_autoupload : minor fixes

--- subroutine_autoupload.R
+++ subroutine_autoupload.R
@@ -1,14 +1,10 @@
 source('uploadOutput.R')
 
 subrt_upload = function(){
-  
-  i = "test"
-  
+    
   repeat{
     
-    message(i)
-    
-    uploadFiles(outFolder = 'localOutput/', drivePath = 'Archive/CT_sim_outputs/')
+    uploadFiles(filePaths = 'localOutput/', drivePath = 'Archive/CT_sim_outputs/')
     
     Sys.sleep(5*60)
     
