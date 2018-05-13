52cdb00613ac2bed98b596da35d0e49cc204af7c
Alec Wong <aw685@cornell.edu>
2018-05-13 10:05:00 -0400

    simSCR.R : Reintroduced data into SIMULATOR function.
    Introduced debugging parameter into runFunc() to keep data during tests, but not during production

--- simSCR.R
+++ simSCR.R
@@ -18,8 +18,7 @@ simSCR<- function(D = 0.83333,lam0=2,sigma=0.50,K=10,X ,buff=3,thinning.rate1 = 
 # thinning.rate2 = thinning rate to convert occupancy data to SCR data
 # grid.space = spacing of state-space grid points used in likelihood evaluation
 # seed = random number seed
-
-
+  
    # Added for reproducibility
    set.seed(seed)
 
@@ -136,16 +135,15 @@ simSCR<- function(D = 0.83333,lam0=2,sigma=0.50,K=10,X ,buff=3,thinning.rate1 = 
    #estimate occupancy p
    sites.used=sum(apply(y.use,2,sum)>0)#sites use at least once
    p.bar=sum(y.occ)/(sites.used*K) #estimated occupancy p
-   out<-list(K=K,n=n,nscap=nscap,sumscap=sumscap,buff=buff,
+   out<-list(y.use=y.use,y.det=y.det,y.occ=y.occ,y.scr=y,s=s,X=X,
+             K=K,n=n,nscap=nscap,sumscap=sumscap,buff=buff,
              psi.bar = mean(psi.grid),p.bar=p.bar,
              N = N, # may as well return this too
-             seed = seed,
-             y.use=y.use,y.det=y.det,y.occ=y.occ,y.scr=y,s=s,X=X, 
-             )
+             seed = seed)
    return(out)
  }
 
-runFunc = function(task){
+runFunc = function(task, debug = T){
     
     # Will source with Ben's SPIM package - otherwise source here.
     if(!require(SPIM)){sourceCpp("intlikRcpp.cpp")}
@@ -192,7 +190,7 @@ runFunc = function(task){
     
     scrAnalysis = function(data){
       
-      scaps = data[['sumscaps']]
+      scaps = data[['sumscap']]
       if(scaps < 3){stop("Insufficient recaptures (scaps < 3) for estimation.")}
       y=apply(data[['y.scr']],c(1,2),sum)
       n=data[['n']]
@@ -274,9 +272,20 @@ runFunc = function(task){
       dir.create("localOutput/")
     }
     
-    out = list(SCR = out.intRcpp, OCC = out.occ, DATA = list(N = scrData$N, K = scrData$K, n = scrData$n, 
-                                                             nscap = scrData$nscap, sumscap = scrData$sumscap, 
-                                                             buff = scrData$buff, psi.bar = scrData$psi.bar, p.bar = scrData$p.bar, seed = scrData$seed))
+    if(debug){
+      
+      out = list(SCR = out.intRcpp, OCC = out.occ, DATA = scrData)
+      
+    }else{
+      
+      out = list(SCR = out.intRcpp, OCC = out.occ, DATA = list(N = scrData$N, K = scrData$K, n = scrData$n,
+                                                               nscap = scrData$nscap, sumscap = scrData$sumscap,
+                                                               buff = scrData$buff, psi.bar = scrData$psi.bar, p.bar = scrData$p.bar, seed = scrData$seed))
+      
+    }
+    
+    
+    
     
     save(out, file = paste0("localOutput/out_task_", task, ".Rdata"))
     
