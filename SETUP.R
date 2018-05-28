36946d91e6050c65626758e1a3e692bf40f69b78
Alec Wong <aw685@cornell.edu>
2018-05-28 14:35:35 -0400

    SETUP.R : Update to include redesign of settings table.
    1. Old settings table is made, for new computers.
    2. New settings table is generated.
    3. Old settings up until task 335742 are retained, the rest eliminated.
    4. MD5 hashes calculated for each setting, to identify uniquely and calculate replicates.
    5. New settings are combined with old settings with a simple rbind.
    6. Replicates are calculated.
    7. New table is written locally.

--- SETUP.R
+++ SETUP.R
@@ -6,26 +6,76 @@ for(i in 1:2){
   # Install and load LOCAL database stuff.
   if(!require(RSQLite)){install.packages('RSQLite')}
   if(!require(dbplyr)){install.packages('dbplyr')}
+  if(!require(dplyr)){install.packages('dplyr')}
   if(!require(Rcpp)){install.packages('Rcpp')}
 }
 
+# Write settings as per iteration 1, for new computers - only if settings.sqlite DNE.
+
 source('writeSettings.R')
 
-# Get settings list
-settings = writeSettings(nreps = 500)
+if(!file.exists('settings.sqlite')){
+  
+  # Get settings list
+  settings = writeSettings_iter_1(nreps = 500)
+  
+  # Have moved away from the .Rdata file.
+  # save(list = c("settings"), file = 'settings.Rdata')
+  
+  # Open local sql connection
+  con = dbConnect(SQLite(), 'settings.sqlite')
+  
+  # Write settings list to connection
+  dbWriteTable(conn = con, name = 'settings', value = settings)
+  
+  # Close connection
+  dbDisconnect(conn = con)
+  
+}
 
-# Have moved away from the .Rdata file.
-# save(list = c("settings"), file = 'settings.Rdata')
 
-# Open local sql connection
+##### Write new settings table as per discussion on May 20. #######
+
+# oldSettings = writeSettings_iter_1(nreps = 1, uniqueOnly = T)
+
+# newSettings = writeSettings_iter_2(nreps = 1, uniqueOnly = T)
+
+newSettings_full = writeSettings_iter_2(nreps = 200)
+
 con = dbConnect(SQLite(), 'settings.sqlite')
 
-# Write settings list to connection
-dbWriteTable(conn = con, name = 'settings', value = settings)
+oldTable = dbReadTable(conn = con, name = 'settings')
 
-# Close connection
-dbDisconnect(conn = con)
+dbDisconnect(con)
 
+# Want everything up until task 335742. This is the last task completed during the pause in Memorial Day weekend.
+
+lastTask = 335742
+
+oldTable_portion = oldTable %>% filter(taskID <= lastTask) %>% select(taskID, nTraps : grid.space)
+oldTable_portion$HASH = apply(X = oldTable_portion[,2:13], MARGIN = 1, FUN = function(x){digest(x, algo = 'md5')})
+
+# Now we want to get everything after task 335742 that matches the new desired settings.
+
+# Rewrite the taskID's to start from the last task + 1
+
+newSettings_full$taskID = seq(lastTask+1, (nrow(newSettings_full)+lastTask))
+
+newSettings_full = newSettings_full %>% select(-settingID, -replicate)
+
+newSettingsTable = rbind.data.frame(oldTable_portion, newSettings_full)
+
+hashOrder = order(newSettingsTable$HASH, newSettingsTable$taskID)
+hashSorted = newSettingsTable$HASH[hashOrder]
+replicateVec = sapply(X = table(hashSorted) %>% as.integer, function(x){seq(1,x)}) %>% do.call(what = c, args = .)
+
+newSettingsTable$replicate[hashOrder] = replicateVec
+
+conLocal = dbConnect(SQLite(), 'settings_v2.sqlite')
+
+dbWriteTable(conn = conLocal, name = 'settings')
+
+dbDisconnect(conLocal)
 
 # Uncomment the following ONLY if you cannot install SPIM via .zip file. Unlikely.
 
