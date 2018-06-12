3f0a623f91547b9ac8a5b76e3a0d462a3c350dae
Alec Wong <aw685@cornell.edu>
2018-06-12 11:18:06 -0400

    subroutine_autoupload.R : Changed handling of uploads. Now the remote set will be downloaded to compare to the local set.

--- subroutine_autoupload.R
+++ subroutine_autoupload.R
@@ -1,9 +1,8 @@
 # Upload to drive, test script
 
-source('autoUploadResource.R')
-
 for(i in 1:2){
-  if(!require(boxr)){install.packages('boxr')}
+  if(!require(boxr)){devtools::install_github('awong234/boxr')}
+  if(!require(dplyr)){install.packages('dplyr')}
 }
 
 ID_lines = readLines(con = 'app.cfg')
@@ -17,25 +16,37 @@ subrt_upload = function(){
     
   repeat{
     
-    x = box_push(dir_id = 48978104905, local_dir = 'localOutput', overwrite = F, delete = F)
+    existing = box_ls_short(dir_id = 48978104905)
+      
+    existing = sapply(X = existing, FUN = `[`, 'name') %>% unlist
     
-    boxr_timediff <- function(x) paste0("took ", format(unclass(x), digits = 3), " ", attr(x, "units"))
+    localFiles = list.files('localOutput/', full.names = T)
     
-    f <- x$file_list
+    localFiles_names = strsplit(localFiles, split = '/') %>% sapply(FUN = `[`, 2)
     
-    tdif <- boxr_timediff(x$end - x$start)
+    toUpload = localFiles[!localFiles_names %in% existing]
     
-    message(paste0("\nboxr ", x$operation, " operation\n\n"))
-    
-    message(paste0(
-      "  User           : ", getOption("boxr.username"), "\n",
-      "  Local dir      : ", x$local_tld,                "\n",
-      "  box.com folder : ", x$box_tld_id,               "\n",
-      "  started at     : ", x$start , " (", tdif, ")",  "\n",
-      "\n"
-    ))
-    
-    message(paste(summarise_ops(x$file_list, x$msg_list)))
+    for(file in toUpload){
+      box_ul(dir_id = 48978104905, file = file, pb = T)
+    }
+
+    # boxr_timediff <- function(x) paste0("took ", format(unclass(x), digits = 3), " ", attr(x, "units"))
+    # 
+    # f <- x$file_list
+    # 
+    # tdif <- boxr_timediff(x$end - x$start)
+    # 
+    # message(paste0("\nboxr ", x$operation, " operation\n\n"))
+    # 
+    # message(paste0(
+    #   "  User           : ", getOption("boxr.username"), "\n",
+    #   "  Local dir      : ", x$local_tld,                "\n",
+    #   "  box.com folder : ", x$box_tld_id,               "\n",
+    #   "  started at     : ", x$start , " (", tdif, ")",  "\n",
+    #   "\n"
+    # ))
+    # 
+    # message(paste(summarise_ops(x$file_list, x$msg_list)))
     
     Sys.sleep(5*60)
     
